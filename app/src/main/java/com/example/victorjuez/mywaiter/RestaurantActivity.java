c7f1bb347bb9c2af0e4736b71089590b3cd25677
Victor <victorjuez.alu@gmail.com>
2018-11-15 20:21:12 +0100

    Simple layout added
    instead of using the scrollable one

--- app/src/main/java/com/example/victorjuez/mywaiter/RestaurantActivity.java
+++ app/src/main/java/com/example/victorjuez/mywaiter/RestaurantActivity.java
@@ -1,11 +1,7 @@
 package com.example.victorjuez.mywaiter;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class RestaurantActivity extends AppCompatActivity {
 
@@ -13,16 +9,5 @@ public class RestaurantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
     }
 }
