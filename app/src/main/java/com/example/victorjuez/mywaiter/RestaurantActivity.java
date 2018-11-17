165c8143ebbc7a2337e63e90e8b63904aa5afc7b
Victor <victorjuez.alu@gmail.com>
2018-11-17 20:43:27 +0100

    CartaActivity created

--- app/src/main/java/com/example/victorjuez/mywaiter/RestaurantActivity.java
+++ app/src/main/java/com/example/victorjuez/mywaiter/RestaurantActivity.java
@@ -1,13 +1,28 @@
 package com.example.victorjuez.mywaiter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RestaurantActivity extends AppCompatActivity {
+
+    private Button verCartaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        verCartaButton = findViewById(R.id.verCartaButton);
+
+        verCartaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RestaurantActivity.this, CartaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
