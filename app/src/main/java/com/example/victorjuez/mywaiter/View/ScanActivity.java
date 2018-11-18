869cfdca552389a46e7c0f3b0f6764b2eefc35a6
Victor <victorjuez.alu@gmail.com>
2018-11-19 00:56:24 +0100

    QR scanner started

--- app/src/main/java/com/example/victorjuez/mywaiter/View/ScanActivity.java
+++ app/src/main/java/com/example/victorjuez/mywaiter/View/ScanActivity.java
@@ -1,22 +1,48 @@
 package com.example.victorjuez.mywaiter.View;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.victorjuez.mywaiter.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class ScanActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
     private Button scanButton;
+
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private SurfaceView cameraView;
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private String token = "";
+    private String tokenanterior = "";
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -52,8 +78,122 @@ public class ScanActivity extends AppCompatActivity {
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ScanActivity.this, RestaurantActivity.class);
-                startActivity(intent);
+
+                //Intent intent = new Intent(ScanActivity.this, RestaurantActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+        barcodeDetector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.QR_CODE)
+                        .build();
+
+        // creo la camara fuente
+        cameraSource = new CameraSource
+                .Builder(getApplicationContext(), barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        cameraView = (SurfaceView) findViewById(R.id.camera_view);
+
+        // listener de ciclo de vida de la camara
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                // verifico si el usuario dio los permisos para la camara
+                if (ActivityCompat.checkSelfPermission(ScanActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        // verificamos la version de ANdroid que sea al menos la M para mostrar
+                        // el dialog de la solicitud de la camara
+                        if (shouldShowRequestPermissionRationale(
+                                Manifest.permission.CAMERA)) ;
+                        requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                MY_PERMISSIONS_REQUEST_CAMERA);
+                    }
+                    return;
+                } else {
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException ie) {
+                        Log.e("CAMERA SOURCE", ie.getMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        // preparo el detector de QR
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() > 0) {
+
+                    // obtenemos el token
+                    token = barcodes.valueAt(0).displayValue.toString();
+
+                    // verificamos que el token anterior no se igual al actual
+                    // esto es util para evitar multiples llamadas empleando el mismo token
+                    if (!token.equals(tokenanterior)) {
+
+                        // guardamos el ultimo token proceado
+                        tokenanterior = token;
+                        Log.i("token", token);
+
+                        if (URLUtil.isValidUrl(token)) {
+                            // si es una URL valida abre el navegador
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(token));
+                            startActivity(browserIntent);
+                        } else {
+                            // comparte en otras apps
+                            Intent shareIntent = new Intent();
+                            shareIntent.setAction(Intent.ACTION_SEND);
+                            shareIntent.putExtra(Intent.EXTRA_TEXT, token);
+                            shareIntent.setType("text/plain");
+                            startActivity(shareIntent);
+                            /*TextView textView = findViewById(R.id.result);
+                            textView.setText(token);*/
+                            cameraSource.stop();
+                            cameraView.
+
+                        }
+
+                        new Thread(new Runnable() {
+                            public void run() {
+                                try {
+                                    synchronized (this) {
+                                        wait(5000);
+                                        // limpiamos el token
+                                        tokenanterior = "";
+                                    }
+                                } catch (InterruptedException e) {
+                                    // TODO Auto-generated catch block
+                                    Log.e("Error", "Waiting didnt work!!");
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
+
+                    }
+                }
             }
         });
     }
